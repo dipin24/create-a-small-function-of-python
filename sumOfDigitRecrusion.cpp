b00f0fd28d6cfdf575b7f38876ef4675f7d1e1e9
Nitin Kumar <56934262+nitinkumar2601@users.noreply.github.com>
2022-10-20 22:50:46 +0530

    Add files via upload

---
+++ sumOfDigitRecrusion.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int digit(int n){
+    if(n==0)
+        return 0;
+
+    return digit(n / 10) + n % 10;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<digit(n);
+}
